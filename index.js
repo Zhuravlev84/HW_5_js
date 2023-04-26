7400b46be9f0ef780cfbe7295ff8d4eefc3d7771
Zhuravlev84 <zyzy84@rambler.ru>
2023-04-26 06:57:15 +0300

    finish_all

--- index.js
+++ index.js
@@ -119,11 +119,11 @@ console.log(products);
 /*Задание 5
 Дано 2 массива. Вам необходимо объединить 2 этих массива, чтобы значения первого массива были ключами, а значения второго массива — значениями.*/
 
-/*const en = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"];
+const en = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"];
 const ru = ["понедельник", "вторник", "среда", "четверг", "пятница", "суббота", "воскресенье"];
 
 const week = {};
 for (let i = 0; i < en.length; i++) {
     week[en[i]] = ru[i];
  }
-console.log(week);*/
+console.log(week);
